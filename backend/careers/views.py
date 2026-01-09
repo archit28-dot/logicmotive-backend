c0dbcad41201113f9e69a00b3e67f7f073c740ef
archit28-dot <architgaikwad28@gmail.com>
2026-01-09 02:44:31 -0800

    Complete backend API with email notifications and jsonresponse refactoring done

--- backend/careers/views.py
+++ backend/careers/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import JobApplication
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.core.mail import EmailMessage
+from django.conf import settings
+
 # Create your views here.
 @csrf_exempt
 def apply_job(request):
@@ -16,35 +19,54 @@ def apply_job(request):
         resume = request.FILES.get('resume')
         # Required field validation
         if not name:
-            return JsonResponse({"error": "Name is required"}, status=400)
+            return JsonResponse({"success":False,"message": "Name is required"}, status=400)
 
         if not email:
-            return JsonResponse({"error": "Email is required"}, status=400)
+            return JsonResponse({"success":False,"message": "Email is required"}, status=400)
 
         if not mobile:
-            return JsonResponse({"error": "Mobile number is required"}, status=400)
+            return JsonResponse({"success":False,"message": "Mobile number is required"}, status=400)
 
         if not experience:
-            return JsonResponse({"error": "Experience is required"}, status=400)
+            return JsonResponse({"success":False,"message": "Experience is required"}, status=400)
 
         if not resume:
-            return JsonResponse({"error": "Resume file is required"}, status=400)
+            return JsonResponse({"success":False,"message": "Resume file is required"}, status=400)
+        
         # Choice validation (VERY IMPORTANT)
         valid_experience = [choice[0] for choice in JobApplication.EXPERIENCE_CHOICES]
         if experience not in valid_experience:
-            return JsonResponse({"error": "Invalid experience value"}, status=400)
+            return JsonResponse({"success":"false","message": "Invalid experience value"}, status=400)
 
         # File type validation
         if not resume.name.lower().endswith('.pdf'):
-            return JsonResponse({"error": "Only PDF resumes are allowed"}, status=400)
+            return JsonResponse({"success":False,"error": "Only PDF resumes are allowed"}, status=400)
         
         # Save valid data
-        JobApplication.objects.create(
+        job=JobApplication.objects.create(
             name=name,
             email=email,
             mobile=mobile,
             experience=experience,
             resume=resume
         )
+        
+        email = EmailMessage(
+                    subject="New Job Application Received",
+                    body=f"""
+                Name: {name}
+                Email: {email}
+                Mobile: {mobile}
+                Experience: {experience}
+                """,
+                    from_email=settings.EMAIL_HOST_USER,
+                    to=[settings.EMAIL_HOST_USER],
+                )
 
-        return JsonResponse({"status": "application submitted"}, status=201)
+        # Attach the PDF resume
+        email.attach_file(job.resume.path)
+
+        email.send(fail_silently=False)
+
+
+        return JsonResponse({"success":True,"status": "application submitted"}, status=201)
--- backend/leads/views.py
+++ backend/leads/views.py
@@ -3,17 +3,19 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Lead
 import json
+from django.core.mail import send_mail
+from django.conf import settings
 
 @csrf_exempt
 def contact_view(request):
     if request.method != 'POST':
-        return JsonResponse({"error":"Invalid request method (only POST allowed)"},status=400)
+        return JsonResponse({"success":False,"error":"Invalid request method (only POST allowed)"},status=400)
     
     try:
         raw_body = request.body.decode("utf-8")
         data=json.loads(raw_body)
     except Exception:
-        return JsonResponse({"error": "Invalid JSON"}, status=400)
+        return JsonResponse({"success":False,"error": "Invalid JSON"}, status=400)
     #extract fields
     name = data.get('name')
     email = data.get('email')
@@ -23,19 +25,19 @@ def contact_view(request):
     message = data.get('message')
     #required field validation
     if not name:
-        return JsonResponse({"error": "Name is required"}, status=400)
+        return JsonResponse({"success":False,"error": "Name is required"}, status=400)
 
     if not email:
-        return JsonResponse({"error": "Email is required"}, status=400)
+        return JsonResponse({"success":False,"error": "Email is required"}, status=400)
 
     if not mobile:
-        return JsonResponse({"error": "Mobile number is required"}, status=400)
+        return JsonResponse({"success":False,"error": "Mobile number is required"}, status=400)
 
     if not company:
-        return JsonResponse({"error": "Company is required"}, status=400)
+        return JsonResponse({"success":False,"error": "Company is required"}, status=400)
 
     if not message:
-        return JsonResponse({"error": "Message is required"}, status=400)
+        return JsonResponse({"success":False,"error": "Message is required"}, status=400)
     
     # Choice validation
     valid_services=[]
@@ -43,7 +45,7 @@ def contact_view(request):
         valid_services.append(choice[0])
         
     if service not in valid_services:
-        return JsonResponse({"error": "Invalid service selected"}, status=400)
+        return JsonResponse({"success":False,"error": "Invalid service selected"}, status=400)
     
     # Saves only VALID data
     Lead.objects.create(
@@ -54,4 +56,19 @@ def contact_view(request):
         service=service,
         message=message
     )
-    return JsonResponse({"status": "success"}, status=201)
+    
+    send_mail(
+    subject="New Contact Form Submission",
+    message=f"""
+    Name: {name}
+    Email: {email}
+    Mobile: {mobile}
+    Company: {company}
+    Service: {service}
+    Message: {message}
+    """,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[settings.EMAIL_HOST_USER],
+        fail_silently=False,
+    )
+    return JsonResponse({"success":True,"status": "success"}, status=201)
